5a8163d073dc354cf1e213d7773683bdbf033e56
Jahdiel C <jcastaneda@belizebank.com>
2023-12-18 08:46:49 -0600

    Initial commit

---
+++ android/src/main/java/intellitrust/belizebank/com/Intellitrust.java
@@ -0,0 +1,11 @@
+package intellitrust.belizebank.com;
+
+import android.util.Log;
+
+public class Intellitrust {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
